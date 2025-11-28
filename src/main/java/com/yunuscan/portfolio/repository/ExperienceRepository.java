45c2af45210c72b4da8dd57ceb1b8b886810a249
yunuscanunal <yunuscanunal1@gmail.com>
2025-11-28 20:44:12 +0300

    Remove unused files and update version to 0.0.3-SNAPSHOT

--- src/main/java/com/yunuscan/portfolio/repository/ExperienceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.yunuscan.portfolio.repository;
-
-import com.yunuscan.portfolio.model.Experience;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExperienceRepository extends JpaRepository<Experience, Long> {
-}
--- src/main/java/com/yunuscan/portfolio/repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package com.yunuscan.portfolio.repository;
-
-import com.yunuscan.portfolio.model.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Long> {
-    // İleride özel sorgular gerekirse buraya eklenir (örn: findByTitle vs.)
-}
